2810ef8566e1baa2938777ef35abc42581447685
ethan-jones <egj77508@uga.edu>
2022-03-06 21:49:43 -0500

    first commit

---
+++ src/Graph.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.util.Scanner;
+
+public class Graph {
+    
+}
